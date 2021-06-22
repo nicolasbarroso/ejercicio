813206c52403211f4b7a43afdf042e0fdc536eaa
Nico Barroso <team@platzi.com>
2021-06-21 22:45:54 -0300

    version 1

---
+++ claseUno.js
@@ -0,0 +1,24 @@
+// const miNombre = "Camada";
+// const apellido = "JavaScript";
+// const numero = 16790;
+// const espacio = " ";
+
+// const concatenacion = miNombre + " " + apellido + " " + numero;
+
+// console.log(concatenacion);
+
+// const edad = parseInt(prompt("Ingrese su edad"));
+// alert("Mi edad es  " + edad);
+
+// const nombre = prompt("Ingrese su nombre");
+// alert("¡Hola, "+ nombre +"!");
+
+// const diez = 10;
+// const numero = parseInt(prompt("Al número que ingreses se le sumará diez!"));
+// const suma = diez + numero;
+// console.log(suma);
+
+const nombre = prompt("Ingresá tu nombre");
+const apellido = prompt("Ingresá tu apellido");
+const concatenacion = nombre +" "+ apellido;
+alert("¡Buenísimo, acaba de ingresar " + concatenacion + "!");
